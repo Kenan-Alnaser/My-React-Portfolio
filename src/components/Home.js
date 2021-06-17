6f64d98a621d21d68f81fa236b97895986ad3a0b
Kenan Alnaser <kenan-na@live.com>
2021-06-17 11:53:12 +0200

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Home = () => {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <div className="mainContainer">
+        <h2>
+          <u>About me:</u>
+          <br /> My Name is Kenan, A junior web developer with skills in:
+          <ul>
+            <li>HTML ✔️</li>
+            <li>CSS ✔️</li>
+            <li>JavaScript ✔️</li>
+            <li>React ✔️</li>
+          </ul>
+          I also enjoy Animations, and games. Which is why I also have a YouTube
+          channel.
+        </h2>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Home;
