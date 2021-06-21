6cdda345c78be9813e148e719f1abdd10a7c906f
Kenan Alnaser <kenan-na@live.com>
2021-06-21 15:03:24 +0200

    responsive design phone and tablet

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,31 +8,39 @@ import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 const Contact = () => {
   return (
     <React.Fragment>
-      <h2>
-        Contact Me:
-        <br />
-        <a
-          href="https://www.linkedin.com/in/kenan-alnaser/"
-          className="contactBtn"
-        >
-          <FontAwesomeIcon icon={faLinkedin} className="contactBtn linkedin" />
-        </a>
-        <a href="https://github.com/Kenan-Alnaser" className="contactBtn">
-          <FontAwesomeIcon icon={faGithub} className="contactBtn github" />
-        </a>
-        <a
-          href="https://www.youtube.com/channel/UCPSKMkfLmi16mXfVyQnOLvA"
-          className="contactBtn"
-        >
-          <FontAwesomeIcon icon={faYoutube} className="contactBtn youtube" />
-        </a>
-        <a
-          href="https://www.facebook.com/VorAnsirtGaming"
-          className="contactBtn"
-        >
-          <FontAwesomeIcon icon={faFacebook} className="contactBtn facebook" />
-        </a>
-      </h2>
+      <div className="contactContainer">
+        <h2>
+          Contact Me:
+          <br />
+          <a
+            href="https://www.linkedin.com/in/kenan-alnaser/"
+            className="contactBtn"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="contactBtn linkedin"
+            />
+          </a>
+          <a href="https://github.com/Kenan-Alnaser" className="contactBtn">
+            <FontAwesomeIcon icon={faGithub} className="contactBtn github" />
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UCPSKMkfLmi16mXfVyQnOLvA"
+            className="contactBtn"
+          >
+            <FontAwesomeIcon icon={faYoutube} className="contactBtn youtube" />
+          </a>
+          <a
+            href="https://www.facebook.com/VorAnsirtGaming"
+            className="contactBtn"
+          >
+            <FontAwesomeIcon
+              icon={faFacebook}
+              className="contactBtn facebook"
+            />
+          </a>
+        </h2>
+      </div>
     </React.Fragment>
   );
 };
