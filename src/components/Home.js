51108b2b8b76cc226c0051605aeff1566aef2a72
Kenan Alnaser <kenan-na@live.com>
2021-06-21 14:31:28 +0200

    fontawesome logos homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHtml5 } from "@fortawesome/free-brands-svg-icons";
+import { faCss3Alt } from "@fortawesome/free-brands-svg-icons";
+import { faJsSquare } from "@fortawesome/free-brands-svg-icons";
+import { faReact } from "@fortawesome/free-brands-svg-icons";
 
 const Home = () => {
   return (
@@ -9,10 +14,18 @@ const Home = () => {
           <br /> My Name is Kenan, A programmer with skills in:
           <ul>
             <li className="listPoint">Web Development: </li>
-            <li>HTML ✔️</li>
-            <li>CSS ✔️</li>
-            <li>JavaScript ✔️</li>
-            <li>React ✔️</li>
+            <li>
+              HTML <FontAwesomeIcon icon={faHtml5} />
+            </li>
+            <li>
+              CSS <FontAwesomeIcon icon={faCss3Alt} />
+            </li>
+            <li>
+              JavaScript <FontAwesomeIcon icon={faJsSquare} />
+            </li>
+            <li>
+              React <FontAwesomeIcon icon={faReact} />
+            </li>
             <li className="listPoint">Android App development 🤖 </li>
           </ul>
           I also enjoy Animations, and games. Which is why I also have a YouTube
