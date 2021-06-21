17c1063a820f2fd9b44ae2d5a91ccf05a85a72b5
Kenan Alnaser <kenan-na@live.com>
2021-06-21 12:09:17 +0200

    invalid page retweaked

--- src/components/Invalid.js
+++ src/components/Invalid.js
@@ -3,7 +3,7 @@ import React from "react";
 const Invalid = () => {
   return (
     <React.Fragment>
-      <h2>You're not supposed to be here</h2>
+      <h2 className="invalidLink">You're not supposed to be here</h2>
     </React.Fragment>
   );
 };
