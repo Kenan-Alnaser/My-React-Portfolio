4073e005fcd5d2c91cc27c2565e4f13d5b5f1494
Kenan Alnaser <kenan-na@live.com>
2021-06-21 12:29:56 +0200

    invalid page image

--- src/components/Invalid.js
+++ src/components/Invalid.js
@@ -3,7 +3,14 @@ import React from "react";
 const Invalid = () => {
   return (
     <React.Fragment>
-      <h2 className="invalidLink">You're not supposed to be here</h2>
+      <h2 className="invalidLink">
+        You're not supposed to be here
+        <img
+          src="https://m.media-amazon.com/images/I/71+Uz-D30oL._SS500_.jpg"
+          alt="404"
+          className="invalidImg"
+        ></img>
+      </h2>
     </React.Fragment>
   );
 };
