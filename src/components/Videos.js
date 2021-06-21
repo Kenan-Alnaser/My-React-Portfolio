ca48b7a59bd0322dc0a3ba6cab783d33abe722d0
Kenan Alnaser <kenan-na@live.com>
2021-06-21 11:51:36 +0200

    videos section

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,7 +1,40 @@
 import React from "react";
 
 const Videos = () => {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <div className="videoContainer">
+        <h1>Some of my Videos:</h1>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/Py60zucuIYQ"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/9yfbOrvEAfE"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/IgYHnbtw2N4"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Videos;
