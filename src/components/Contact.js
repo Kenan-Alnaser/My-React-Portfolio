c269cc47aa43c72b717c6793870873e94ec1c55b
Kenan Alnaser <kenan-na@live.com>
2021-06-21 14:25:56 +0200

    fontawesome contact links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,38 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 
 const Contact = () => {
   return (
     <React.Fragment>
-      <h2>Contact Me</h2>
+      <h2>
+        Contact Me:
+        <br />
+        <a
+          href="https://www.linkedin.com/in/kenan-alnaser/"
+          className="contactBtn"
+        >
+          <FontAwesomeIcon icon={faLinkedin} className="contactBtn linkedin" />
+        </a>
+        <a href="https://github.com/Kenan-Alnaser" className="contactBtn">
+          <FontAwesomeIcon icon={faGithub} className="contactBtn github" />
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UCPSKMkfLmi16mXfVyQnOLvA"
+          className="contactBtn"
+        >
+          <FontAwesomeIcon icon={faYoutube} className="contactBtn youtube" />
+        </a>
+        <a
+          href="https://www.facebook.com/VorAnsirtGaming"
+          className="contactBtn"
+        >
+          <FontAwesomeIcon icon={faFacebook} className="contactBtn facebook" />
+        </a>
+      </h2>
     </React.Fragment>
   );
 };
